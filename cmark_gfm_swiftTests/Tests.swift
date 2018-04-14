4ed3d7436ed3e352bb4860aa2d7d216bac14883f
Ryan Nystrom <rnystrom@instagram.com>
2018-04-14 08:46:34 -0400

    add failing check tests

--- cmark_gfm_swiftTests/Tests.swift
+++ cmark_gfm_swiftTests/Tests.swift
@@ -16,12 +16,19 @@ extension TextElement {
     }
 }
 
+extension String {
+    func substring(with nsrange: NSRange) -> String? {
+        guard let range = Range(nsrange, in: self) else { return nil }
+        return String(self[range])
+    }
+}
+
 extension Sequence where Iterator.Element == TextElement {
     var string: String { return reduce("") { $0 + $1.string } }
 }
 
 class Tests: XCTestCase {
-    
+
     func testMarkdownToHTML() {
         let markdown = "*Hello World*"
         let html = markdownToHtml(string: markdown)
@@ -222,22 +229,107 @@ class Tests: XCTestCase {
         guard case .text(let t1) = elements[2] else { fatalError() }
         XCTAssertEqual(t1.string, "Lorem ipsum dolor sit amet.")
 
-        guard case .list(let i1, _) = elements[3] else { fatalError() }
+        guard case let .list(i1, _) = elements[3] else { fatalError() }
         XCTAssertEqual(i1.count, 2)
         XCTAssertEqual(i1[0].count, 1)
         XCTAssertEqual(i1[1].count, 2)
 
-        guard case .list(let n1, _, let nl1) = i1[1][1] else { fatalError() }
+        guard case let .list(n1, _, nl1) = i1[1][1] else { fatalError() }
         XCTAssertEqual(n1.count, 2)
         XCTAssertEqual(nl1, 1)
 
-        guard case .quote(let q1, let ql1) = elements[4] else { fatalError() }
+        guard case let .quote(q1, ql1) = elements[4] else { fatalError() }
         XCTAssertEqual(q1.string, "Quote")
         XCTAssertEqual(ql1, 1)
 
-        guard case .quote(let q2, let ql2) = elements[5] else { fatalError() }
+        guard case let .quote(q2, ql2) = elements[5] else { fatalError() }
         XCTAssertEqual(q2.string, "Quote 2")
         XCTAssertEqual(ql2, 2)
+
+        guard case .list(let i2, _) = elements[6] else { fatalError() }
+        XCTAssertEqual(i2.count, 2)
+        XCTAssertEqual(i2[0].count, 1)
+        XCTAssertEqual(i2[1].count, 1)
+
+        guard case .text(let ct1) = i2[0][0] else { fatalError() }
+        XCTAssertEqual(ct1.count, 2)
+
+        guard case let .checkbox(cb1, cr1) = ct1[0] else { fatalError() }
+        XCTAssertFalse(cb1)
+        XCTAssertEqual(cr1.location, 155)
+        XCTAssertEqual(cr1.length, 3)
+        XCTAssertEqual(markdown.substring(with: cr1), "[ ]")
+
+        guard case .text(let ct2) = i2[1][0] else { fatalError() }
+        XCTAssertEqual(ct2.count, 2)
+
+        guard case let .checkbox(cb2, cr2) = ct2[0] else { fatalError() }
+        XCTAssertTrue(cb2)
+        XCTAssertEqual(cr2.location, 171)
+        XCTAssertEqual(cr2.length, 3)
+        XCTAssertEqual(markdown.substring(with: cr2), "[x]")
+    }
+
+    func test_simpleLists() {
+        let markdown = """
+          paragraph
+          - [ ] not checked
+          - [x] checked
+          """
+        let elements = Node(markdown: markdown)!.flatElements
+        XCTAssertEqual(elements.count, 2)
+
+        guard case .list(let l, _) = elements[1] else { fatalError() }
+        XCTAssertEqual(l.count, 2)
+        XCTAssertEqual(l[0].count, 1)
+        XCTAssertEqual(l[1].count, 1)
+
+        guard case .text(let t1) = l[0][0] else { fatalError() }
+        XCTAssertEqual(t1.count, 2)
+
+        guard case let .checkbox(cb1, cr1) = t1[0] else { fatalError() }
+        XCTAssertFalse(cb1)
+        XCTAssertEqual(cr1.location, 12)
+        XCTAssertEqual(cr1.length, 3)
+        XCTAssertEqual(markdown.substring(with: cr1), "[ ]")
+
+        guard case .text(let t2) = l[1][0] else { fatalError() }
+        XCTAssertEqual(t2.count, 2)
+
+        guard case let .checkbox(cb2, cr2) = t2[0] else { fatalError() }
+        XCTAssertTrue(cb2)
+        XCTAssertEqual(cr2.location, 30)
+        XCTAssertEqual(cr2.length, 3)
+        XCTAssertEqual(markdown.substring(with: cr2), "[x]")
+    }
+
+    func test_nestedLists() {
+        let markdown = "First unordered list item\r\n- Another item\r\n  * Unordered sub-list. \r\n\r\n1. Actual numbers don't matter, just that it's a number\r\n    1. Ordered sub-list\r\n4. And another item.\r\n\r\n* Unordered list can use asterisks\r\n- Or minuses\r\n+ Or pluses\r\n\r\n- [x] And checked boxes\r\n- [ ] Or unchecked"
+        let elements = Node(markdown: markdown)!.flatElements
+        XCTAssertEqual(elements.count, 7)
+
+        guard case .list(let l, _) = elements[6] else { fatalError() }
+        XCTAssertEqual(l.count, 2)
+        XCTAssertEqual(l[0].count, 1)
+        XCTAssertEqual(l[1].count, 1)
+
+        guard case .text(let t1) = l[0][0] else { fatalError() }
+        XCTAssertEqual(t1.count, 2)
+
+        guard case let .checkbox(cb1, cr1) = t1[0] else { fatalError() }
+        XCTAssertTrue(cb1)
+        XCTAssertEqual(cr1.location, 244) // +4
+        XCTAssertEqual(cr1.length, 3)
+        XCTAssertEqual(markdown.substring(with: cr1), "[x]")
+
+        guard case .text(let t2) = l[1][0] else { fatalError() }
+        XCTAssertEqual(t2.count, 2)
+
+        guard case let .checkbox(cb2, cr2) = t2[0] else { fatalError() }
+        XCTAssertFalse(cb2)
+        XCTAssertEqual(cr2.location, 269)
+        XCTAssertEqual(cr2.length, 3)
+        XCTAssertEqual(markdown.substring(with: cr2), "[ ]")
     }
 
     func testComplicatedLists() {
@@ -301,3 +393,4 @@ class Tests: XCTestCase {
     }
 
 }
+
